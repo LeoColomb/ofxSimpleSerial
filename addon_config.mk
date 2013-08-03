4ce555d1c39e5e425db3331b90e74ea59b6f458a
LeoColomb <leocolombaro@yahoo.fr>
2013-08-03 17:57:56 +0200

    Move install.xml into addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+﻿meta:
+	ADDON_NAME = ofxSimpleSerial
+	ADDON_DESCRIPTION = Simplifies Serial Communication from openFrameworks
+	ADDON_AUTHOR = Leo Colombaro / Peter Uithoven and Kostas Skenterlis
+	ADDON_TAGS = "serial" "arduino" "communication"
+	ADDON_URL = https://github.com/LeoColomb/ofxSimpleSerial
